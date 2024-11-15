61496af20e6ee4d525db6c83e01f451bbfc92013
Ismail <Ismail@LAPTOP-A344PTEH>
2024-11-15 17:04:09 +0530

    test

--- src/com/example/Simple_Test.java
+++ src/com/example/Simple_Test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Simple_Test {
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/usr/binchromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.google.com");
 		Thread.sleep(5000);
